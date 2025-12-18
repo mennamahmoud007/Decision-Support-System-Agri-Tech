475d77b6875857f3fa602fc8b280d868c758ad13
yasmin <yasminsherif2028@gmail.com>
2025-12-19 00:00:46 +0200

    crop recommendation added (in-complete)

--- frontend/script.js
+++ frontend/script.js
@@ -3,3 +3,36 @@ card1Button.addEventListener('click', () => {
     const section = document.getElementById('aboutus');
     section.scrollIntoView({behavior: 'smooth'});
 })
+
+const form = document.getElementById('crop-form');
+if(form){
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const data = {
+            crop: document.getElementById('crop').value,
+            temp: parseFloat(document.getElementById('temp').value),
+            humidity: parseFloat(document.getElementById('humidity').value),
+            rainfall: parseFloat(document.getElementById('rainfall').value),
+            N: parseFloat(document.getElementById('N').value),
+            P: parseFloat(document.getElementById('P').value),
+            K: parseFloat(document.getElementById('K').value),
+            ph: parseFloat(document.getElementById('ph').value)
+        };
+        try{
+            const res = await fetch('http://127.0.0.1:5000', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify(data)
+            });
+
+            const result = await res.json();
+            document.getElementById('result').innerHTML = `
+                <h3>Recommended Crop</h3>
+                <pre>${JSON.stringify(result, null, 2)}</pre>
+                `;
+        }   catch (err) {
+            document.getElementById('result').textContent = 'Error connecting to server!';
+            console.error(err);
+        }
+    });
+};
