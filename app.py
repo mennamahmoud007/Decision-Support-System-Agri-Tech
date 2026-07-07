48dd2135268738e93797392a35fe2126f76a1da0
yasmin <yasminsherif2028@gmail.com>
2026-07-07 12:18:38 +0300

    feat: add requirements file for deplyment

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from rules import generate_recommendation
 from flask_cors import CORS
- 
-app = Flask(__name__)
+import os
+
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
- 
+
+@app.route("/")
+def index():
+    return send_from_directory('frontend', 'index.html')
+
+@app.route("/form")
+def form():
+    return send_from_directory('frontend', 'form.html')
+
 @app.route("/recommend", methods=["POST"])
 def recommend():
     data = request.get_json()
@@ -12,6 +21,6 @@ def recommend():
         return jsonify({"error": "No data received"}), 400
     recommendation = generate_recommendation(data)
     return jsonify(recommendation)
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
